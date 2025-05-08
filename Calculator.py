a6e91b80db6e7d440838606edfeb8cf37051fdfd
devhansrohini <115444637+devhansrohini@users.noreply.github.com>
2025-05-08 21:42:24 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,16 @@
+n1=int(input("Enter first number: "))
+n2=int(input("Enter second number: "))
+sum=n1+n2
+print("The sum is",sum)
+dif=n1-n2
+print("The difference is:",dif)
+mul=n1*n2
+print("The product is:",mul)
+div=n1/n2
+print("The division is:",div)
+rem=n1%n2
+print("The remainder is:",rem)
+mod=n1//n2
+print("The modulo is:",mod)
+exp=n1**n2
+print("The exponential is:",exp)
